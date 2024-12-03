a55d01027ad3e499976d950793ad4cf478958dcb
HemaMaruthi <Smaruthi.mahi111@gmail.com>
2024-12-03 07:15:27 +0530

    cd

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        await mongoose.connect(process.env.MONGODB_CONNECT_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000 
+        })
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
